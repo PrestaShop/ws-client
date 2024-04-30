732a2e54b68ce0e34e33d0dd0135788270109b39
dparis <david.paris@prestashop.com>
2024-04-30 14:25:58 +0200

    lib

---
+++ src/types/index.ts
@@ -0,0 +1,5 @@
+export * as config from './config.type';
+export * as entityMapping from './entity-mapping.type';
+export * as category from './ws-entities/categories.type';
+export * as order from './ws-entities/orders.type';
+export * as product from './ws-entities/products.type';
